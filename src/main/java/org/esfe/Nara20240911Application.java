0b727b58d270dbe8fb2d79ad0b6bacfca4055b08
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-11 20:43:34 -0600

    Inicio del proyecto

---
+++ src/main/java/org/esfe/Nara20240911Application.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nara20240911Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nara20240911Application.class, args);
+	}
+
+}
